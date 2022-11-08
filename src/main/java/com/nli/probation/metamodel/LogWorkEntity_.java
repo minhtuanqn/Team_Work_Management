c029bf8ad5cf4dd081f48b938d254c0427055235
Tuan Minh Le <tuanmle@kms.com.vn>
2022-11-08 15:11:17 +0700

    update

--- src/main/java/com/nli/probation/metamodel/LogWorkEntity_.java
+++ src/main/java/com/nli/probation/metamodel/LogWorkEntity_.java
@@ -2,7 +2,7 @@ package com.nli.probation.metamodel;
 
 public class LogWorkEntity_ {
     public static final String ID = "id";
-    public static final String TASK_ID = "taskId";
+    public static final String TASK_ENTITY = "taskEntity";
     public static final String START_TIME = "startTime";
     public static final String END_TIME = "endTime";
     public static final String STATUS = "status";
--- src/main/java/com/nli/probation/metamodel/TaskEntity_.java
+++ src/main/java/com/nli/probation/metamodel/TaskEntity_.java
@@ -5,7 +5,7 @@ public class TaskEntity_ {
     public static final String TITLE = "title";
     public static final String DESCRIPTION = "description";
     public static final String ESTIMATED_TIME = "estimatedTime";
-    public static final String ASSIGNEE_ID = "assigneeId";
+    public static final String USER_ACCOUNT_ENTITY = "userAccountEntity";
     public static final String ACTUAL_TIME = "actualTime";
     public static final String STATUS = "status";
 }
