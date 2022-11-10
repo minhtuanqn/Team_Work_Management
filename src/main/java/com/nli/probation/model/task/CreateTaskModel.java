a8700d675a5eced4de63cb9aeca8a5183c0f048b
Tuan Minh Le <tuanmle@kms.com.vn>
2022-11-10 09:48:42 +0700

    finish-validate-task

--- src/main/java/com/nli/probation/model/task/CreateTaskModel.java
+++ src/main/java/com/nli/probation/model/task/CreateTaskModel.java
@@ -4,18 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter
 public class CreateTaskModel {
 
+    @NotNull(message = "{task_title.null}")
+    @Length(message = "{task_title.length}", min = 1, max = 500)
     private String title;
 
+    @NotNull(message = "{task_description.null}")
+    @Length(message = "{task_description.length}", min = 1, max = 1000)
     private String description;
 
+    @NotNull(message = "{task_starttime.null}")
     private LocalDateTime startTime;
 
+    @NotNull(message = "{task_estimatedtime.null}")
     private double estimatedTime;
 
     private int assigneeId;
--- src/main/java/com/nli/probation/model/task/UpdateTaskModel.java
+++ src/main/java/com/nli/probation/model/task/UpdateTaskModel.java
@@ -4,24 +4,32 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter
 public class UpdateTaskModel {
+    @NotNull(message = "{task_id.null}")
     private int id;
 
+    @NotNull(message = "{task_title.null}")
+    @Length(message = "{task_title.length}", min = 1, max = 500)
     private String title;
 
+    @NotNull(message = "{task_description.null}")
+    @Length(message = "{task_description.length}", min = 1, max = 1000)
     private String description;
 
+    @NotNull(message = "{task_starttime.null}")
     private LocalDateTime startTime;
 
+    @NotNull(message = "{task_estimatedtime.null}")
     private double estimatedTime;
 
     private int assigneeId;
 
-    private double actualTime;
-
+    @NotNull(message = "{task_status.null}")
     private int status;
 }
