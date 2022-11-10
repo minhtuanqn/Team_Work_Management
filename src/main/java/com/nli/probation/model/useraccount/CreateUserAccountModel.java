03472a1c2ee742afd36b63abc1b83879585fcae6
Tuan Minh Le <tuanmle@kms.com.vn>
2022-11-10 10:09:42 +0700

    finish-validate-account

--- src/main/java/com/nli/probation/model/useraccount/CreateUserAccountModel.java
+++ src/main/java/com/nli/probation/model/useraccount/CreateUserAccountModel.java
@@ -5,18 +5,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter
 public class CreateUserAccountModel {
 
+    @NotNull(message = "{account_name.null}")
     private String name;
 
+    @NotNull(message = "{account_email.null}")
     private String email;
 
+    @NotNull(message = "{account_phone.null}")
     private String phone;
 
     private int teamId;
 
+    @NotNull(message = "{office_id.null}")
     private int officeId;
 
+    @NotNull(message = "{role_id.null}")
     private int roleId;
 }
--- src/main/java/com/nli/probation/model/useraccount/UpdateUserAccountModel.java
+++ src/main/java/com/nli/probation/model/useraccount/UpdateUserAccountModel.java
@@ -5,21 +5,30 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter
 public class UpdateUserAccountModel {
+    @NotNull(message = "{account_id.null}")
     private int id;
 
+    @NotNull(message = "{account_name.null}")
     private String name;
 
+    @NotNull(message = "{account_email.null}")
     private String email;
 
+    @NotNull(message = "{account_phone.null}")
     private String phone;
 
     private int teamId;
 
+    @NotNull(message = "{office_id.null}")
     private int officeId;
 
+    @NotNull(message = "{role_id.null}")
     private int roleId;
 
+    @NotNull(message = "{account_status.null}")
     private int status;
 }
