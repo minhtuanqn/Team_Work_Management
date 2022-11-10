d5567e1ec42487de9d5798f6a434de83697f819f
Tuan Minh Le <tuanmle@kms.com.vn>
2022-11-10 09:55:59 +0700

    finish-validate-log-work

--- src/main/java/com/nli/probation/model/logwork/CreateLogWorkModel.java
+++ src/main/java/com/nli/probation/model/logwork/CreateLogWorkModel.java
@@ -6,14 +6,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter
 public class CreateLogWorkModel {
 
+    @NotNull(message = "{task_id.null}")
     private int taskId;
 
+    @NotNull(message = "{logwork_starttime.null}")
     private LocalDateTime startTime;
 
+    @NotNull(message = "{logwork_endtime.null}")
     private LocalDateTime endTime;
 }
--- src/main/java/com/nli/probation/model/logwork/UpdateLogWorkModel.java
+++ src/main/java/com/nli/probation/model/logwork/UpdateLogWorkModel.java
@@ -6,18 +6,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter
 public class UpdateLogWorkModel {
 
+    @NotNull(message = "{logwork_id.null}")
     private int id;
 
+    @NotNull(message = "{logwork_starttime.null}")
     private LocalDateTime startTime;
 
+    @NotNull(message = "{logwork_endtime.null}")
     private LocalDateTime endTime;
 
+    @NotNull(message = "{logwork_status.null}")
     private int status;
 
+    @NotNull(message = "{task_id.null}")
     private int taskId;
 }
