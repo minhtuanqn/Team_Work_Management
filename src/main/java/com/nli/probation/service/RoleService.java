84c1687aee66d82adc52f5b7a9b90e2038a00b3a
Tuan Minh Le <tuanmle@kms.com.vn>
2022-11-14 21:03:39 +0700

    finish-update-role

--- src/main/java/com/nli/probation/service/RoleService.java
+++ src/main/java/com/nli/probation/service/RoleService.java
@@ -79,6 +79,8 @@ public class RoleService {
         //Find role by id
         Optional<RoleEntity> deletedRoleOptional = roleRepository.findById(id);
         RoleEntity deletedRoleEntity = deletedRoleOptional.orElseThrow(() -> new NoSuchEntityException("Not found role with id"));
+        if(deletedRoleEntity.getStatus() == EntityStatusEnum.RoleStatusEnum.DISABLE.ordinal())
+            throw new NoSuchEntityException("This role was deleted");
 
         //Set status for entity
         deletedRoleEntity.setStatus(EntityStatusEnum.RoleStatusEnum.DISABLE.ordinal());
